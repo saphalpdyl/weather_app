c7212bbd45577f5995e6e548e12164f34acd0746
saphalpdyl <saphalpdyl@gmail.com>
2024-01-11 21:06:01 +0545

    added note

--- _16_weather_app/settings.py
+++ _16_weather_app/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*"
+]
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
